777b56d8b3f553ec7b8b1441d5dea58de2131b50
Han-Bin Song <thdgksq@naver.com>
2023-02-07 18:30:59 +0900

    Add #1 - view controllers
    5가지 뷰 컨트롤러 작성

---
+++ src/main/java/com/zinzza/getinline/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.zinzza.getinline.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController implements ErrorController {
+
+    // 시작 페이지 매핑
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    // 에러 표시 페이지 매핑
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
+
+}
---
+++ src/main/java/com/zinzza/getinline/controller/PlaceController.java
@@ -0,0 +1,23 @@
+package com.zinzza.getinline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/places")
+@Controller
+public class PlaceController {
+
+    @GetMapping("/")
+    public String places() {
+        return "place/index";
+    }
+
+    @GetMapping("/{placeId}")
+    public String placesDetail(@PathVariable Integer placeId) {
+        return "place/detail";
+    }
+
+
+}
