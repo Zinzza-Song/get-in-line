c8798a6e2dc3baf7927d58848f54cd300ac7a6db
Han-Bin Song <thdgksq@naver.com>
2023-02-07 21:45:19 +0900

    Add #1 - error code, exception, api responses
    프로젝트에서 사용할 에러 코드와 예외, 공통 응답 객체 설계
    직접 정의한 예외를 사용함으로써 프로젝트에서 예상한 문제와 예상치 못한 문제를 분리하고자함

--- src/main/java/com/zinzza/getinline/controller/BaseController.java
+++ src/main/java/com/zinzza/getinline/controller/BaseController.java
@@ -2,10 +2,12 @@ package com.zinzza.getinline.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@ControllerAdvice(basePackageClasses = BaseController.class)
 public class BaseController implements ErrorController {
 
     // 시작 페이지 매핑
