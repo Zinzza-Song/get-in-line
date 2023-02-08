a339f184438ceaa02c0eec78cfbee8facc710a1d
Han-Bin Song <thdgksq@naver.com>
2023-02-08 21:04:43 +0900

    Set #1 - TDD Setting
    TDD를 위한 프로젝트 세팅

--- src/test/java/com/zinzza/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/zinzza/getinline/controller/BaseControllerTest.java
@@ -1,6 +1,5 @@
 package com.zinzza.getinline.controller;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BaseController.class)
@@ -22,14 +23,15 @@ class BaseControllerTest {
 
     @DisplayName("[view][GET] 기본 페이지 요청")
     @Test
-    void basePageShouldShowIndexPage() throws Exception {
+    void givenNothing_whenRequestingRootPage_thenReturnsIndexPage() throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(content().string(Matchers.containsString("This is default page.")))
-                .andExpect(view().name("index"));
+                .andExpect(content().string(containsString("This is default page.")))
+                .andExpect(view().name("index"))
+                .andDo(print());
     }
 }
